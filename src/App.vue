098d90c687447e9eee9e3f7677d2fc6230abbad9
Sayak Chakraborty <44857480+sayak-chak@users.noreply.github.com>
2021-05-25 22:32:25 +0530

    WIP | Refactor main view

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,83 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark />
-    <v-main>
-      <!-- <MessagingInterface v-if="this.$store.state.isLoggedIn" />
+  <v-app id="app">
+    <v-app-bar app color="rgb(234, 239, 245)" dark>
+      <v-img
+        class="mx-2"
+        src="../public/logo.png"
+        max-height="40"
+        max-width="40"
+        contain
+      ></v-img>
+    </v-app-bar>
+    <v-main id="main-app">
+      <MessagingPortal
+        :websocket="this.websocket"
+        v-if="this.$store.state.isLoggedIn"
+      />
       <div v-else>
-        <v-text-field
-          type="text"
-          placeholder="username"
-          id="username"
-        ></v-text-field>
-        <v-text-field
-          type="password"
-          placeholder="password"
-          id="password"
-        ></v-text-field>
-        <v-btn @click="authenticateUser()">Login</v-btn>
-        <v-btn @click="registerUser()">Sign Up</v-btn>
-      </div> -->
-      <MessagingPortal />
+        <v-row justify="center" align="center">
+          <v-col cols="10" xs="12" sm="8" md="6">
+            <v-row justify="center" align="center" id="login-register-form">
+              <v-col cols="12">
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      type="text"
+                      placeholder="username"
+                      id="username"
+                      outlined
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      type="password"
+                      placeholder="password"
+                      id="password"
+                      outlined
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-col>
+              <v-col cols="12">
+                <v-row>
+                  <v-col cols="12" xs="12" sm="5">
+                    <v-btn
+                      rounded
+                      color="black"
+                      dark
+                      @click="authenticateUser()"
+                      id="login-button"
+                      >Login
+                    </v-btn>
+                  </v-col>
+                  <v-col cols="0" xs="0" sm="2"></v-col>
+                  <v-col cols="12" xs="12" sm="5">
+                    <v-btn
+                      rounded
+                      color="black"
+                      dark
+                      @click="registerUser()"
+                      id="register-button"
+                      >Signup</v-btn
+                    >
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import MessagingInterface from "./components/MessagingInterface.vue";
 import MessagingPortal from "./components/MessagingPortal.vue";
 import config from "./../config.json";
 
 @Component({
   components: {
-    MessagingInterface,
     MessagingPortal,
   },
 })
@@ -40,17 +87,25 @@ export default class App extends Vue {
     document.title = "Chat App";
   }
 
-  created(): void {
-    if (this.$cookies.isKey("AUTH-TOKEN")) {
-      this.$http
-        .post(config.API_HTTPS + "/logincheck", {
-          "AUTH-TOKEN": this.$cookies.get("AUTH-TOKEN"),
-        })
-        .then(
-          (response) => console.log(response.data),
-          (error) => console.log(error)
+  mounted(): void {
+    this.$http.get(config.API_HTTPS + "/logincheck").then(
+      (response) => {
+        this.websocket = new WebSocket(
+          config.API_WS +
+            "/chat" +
+            "?sender=" +
+            this.$store.state.username +
+            "&auth=" +
+            response.data.auth
         );
-    }
+        if (typeof response.data.username === "string") {
+          this.loadMessagingPortal(response.data.username);
+        } else {
+          //TODO: handle sad paths
+        }
+      },
+      (error) => console.log(error)
+    );
   }
 
   registerUser(): void {
@@ -58,18 +113,22 @@ export default class App extends Vue {
       .value;
     let password = (document.getElementById("password") as HTMLInputElement)
       .value;
-    this.websocket = new WebSocket(
-      config.API_WS + "/register?username=" + username + "&password=" + password
-    );
 
-    this.websocket.addEventListener("message", (registerResponseJSON) => {
-      let registerResponse = JSON.parse(registerResponseJSON.data);
-      if (registerResponse.isRequestSuccessful) {
-        this.loadChattingInterface(username);
-      } else {
-        console.log("FAIL ", registerResponse); //TODO: handle sad paths
-      }
-    });
+    this.$http
+      .post(config.API_HTTPS + "/register", {
+        username: username,
+        password: password,
+      })
+      .then(
+        (registerResponseJSON) => {
+          if (registerResponseJSON.data.isRequestSuccessful) {
+            this.loadMessagingPortal(username);
+          } else {
+            //TODO: handle sad paths
+          }
+        },
+        (error) => console.log(error) //TODO: handle sad paths
+      );
   }
 
   authenticateUser(): void {
@@ -77,29 +136,59 @@ export default class App extends Vue {
       .value;
     let password = (document.getElementById("password") as HTMLInputElement)
       .value;
-    this.websocket = new WebSocket(
-      config.API_WS + //TODO: move to config files
-        "/login?username=" +
-        username +
-        "&password=" +
-        password
-    );
-
-    this.websocket.addEventListener("message", (registerResponseJSON) => {
-      let registerResponse = JSON.parse(registerResponseJSON.data);
-      if (registerResponse.isRequestSuccessful) {
-        this.loadChattingInterface(username);
-      } else {
-        console.log("FAIL ", registerResponse); //TODO: handle sad paths
-      }
-    });
-    console.log("Reg");
+    // let options = {
+    //   headers: {
+    //     "ACCESS-CONTROL-ALLOW-ORIGIN": config.API_HTTPS,
+    //     "Content-Type": "Application/json",
+    //   },
+    //   credentials: true,
+    // };
+    this.$http
+      .post(
+        config.API_HTTPS + "/login",
+        {
+          username: username,
+          password: password,
+        }
+        // options
+      )
+      .then(
+        (registerResponseJSON) => {
+          if (registerResponseJSON.data.isRequestSuccessful) {
+            this.loadMessagingPortal(username);
+          } else {
+            //TODO: handle sad paths
+          }
+        },
+        (error) => console.log(error) //TODO: handle sad paths
+      );
   }
 
-  loadChattingInterface(username: string): void {
-    this.$store.commit("setUsername", username); //TODO: refactor to cookie/auth token system
+  loadMessagingPortal(username: string): void {
+    this.$store.commit("setUsername", username);
     this.$store.commit("loginUser");
-    this.$store.commit("loadChattingPortal");
   }
 }
 </script>
+
+<style scoped>
+#app {
+  background-color: rgb(169, 204, 250);
+}
+#login-register-form {
+  margin-top: 30%;
+  border: 2px solid black;
+  border-radius: 10px;
+  background-color: rgb(212, 215, 219);
+}
+#main-app {
+  margin: 10px;
+  margin-top: 30px;
+}
+#login-button {
+  width: 100%;
+}
+#register-button {
+  width: 100%;
+}
+</style>
